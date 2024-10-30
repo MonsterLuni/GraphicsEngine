3eb8ac933889d813a04cf0794c14881bdd59c006
Luca Moser <luca.moser@bict-edu.ch>
2024-10-30 16:19:00 +0100

    you can now draw something on the window

--- src/window.rs
+++ src/window.rs
@@ -2,15 +2,14 @@ use std::ptr::{null, null_mut};
 use std::thread;
 use std::time::Duration;
 use winapi::ctypes::c_int;
-use winapi::um::winuser::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetDC, GetMessageW, RegisterClassW, ShowWindow, TranslateMessage, UpdateWindow, MSG, SW_SHOW, WNDCLASSW, WS_OVERLAPPEDWINDOW};
+use winapi::um::winuser::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetDC, GetMessageW, RegisterClassW, ReleaseDC, ShowWindow, TranslateMessage, UpdateWindow, MSG, SW_SHOW, WNDCLASSW, WS_OVERLAPPEDWINDOW};
 use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::shared::windef::{HWND};
-use winapi::um::wingdi::{LineTo, Rectangle};
 use winapi::shared::minwindef::{LRESULT, WPARAM, LPARAM, HINSTANCE};
+use winapi::um::wingdi::LineTo;
+
 extern "system" fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    match msg{
-        _ => unsafe { DefWindowProcW(hwnd, msg, w_param, l_param) }
-    }
+    unsafe { DefWindowProcW(hwnd, msg, w_param, l_param) }
 }
 fn to_w_string(s: &str) -> Vec<u16> {
     let mut v: Vec<u16> = s.encode_utf16().collect();
@@ -43,28 +42,30 @@ impl Window {
             cbWndExtra: 0,
             lpszMenuName: null_mut(),
         };
-        Self { name, class_name, pos_x, pos_y, height, width, class, h_instance }
+        unsafe { RegisterClassW(&class) };
+        let hwnd:HWND = unsafe { CreateWindowExW(0, to_w_string(&class_name).as_ptr(), to_w_string(&name).as_ptr(), WS_OVERLAPPEDWINDOW, pos_x as c_int, pos_y as c_int, width as c_int, height as c_int, null_mut(), null_mut(), h_instance, null_mut()) };
+        Self { name, class_name, pos_x, pos_y, height, width, class, h_instance, hwnd }
     }
     pub fn show(&self) {
-        unsafe { RegisterClassW(&self.class) };
-        let hwnd:HWND = unsafe { CreateWindowExW(0, to_w_string(&self.class_name).as_ptr(), to_w_string(&self.name).as_ptr(), WS_OVERLAPPEDWINDOW, self.pos_x as c_int, self.pos_y as c_int, self.width as c_int, self.height as c_int, null_mut(), null_mut(), self.h_instance, null_mut()) };
         unsafe {
-            ShowWindow(hwnd, SW_SHOW);
-            UpdateWindow(hwnd);
+            ShowWindow(self.hwnd, SW_SHOW);
+            UpdateWindow(self.hwnd);
         };
         let mut msg: MSG = unsafe { std::mem::zeroed() };
         while unsafe { GetMessageW(&mut msg, null_mut(), 0, 0) } > 0 {
             unsafe {
                 TranslateMessage(&msg);
-                UpdateWindow(hwnd);
                 DispatchMessageW(&msg);
+                self.draw()
             };
             thread::sleep(Duration::from_millis(1));
         };
     }
     pub fn draw(&self){
         unsafe{
-            let hdc = GetDC(self);
+            let hdc = GetDC(self.hwnd);
+            LineTo(hdc,100,100);
+            ReleaseDC(self.hwnd, hdc);
         }
     }
 }
