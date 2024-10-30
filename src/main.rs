9f79584acccb7488258e51c6b7465c1cf2165c00
MonsterLuni <monsterluni@gmail.com>
2024-10-30 20:27:01 +0100

    add draw_rectangle so now you can draw rectangles

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ fn main() {
     let window: Window = Window::new("Heureka".to_string(), "Class".to_string(), 100, 100, 200, 200);
     window.show();
 
-    window.draw_line(&Point{x: 10,y: 50},Point{x: 10,y: 100});
-    window.change_pencil(2,Color{r: 255,g: 50,b: 20});
-    window.draw_rectangle(&Point{x: 50,y: 50},50,50);
+    window.draw_line(&Point{x: 10,y: 50},&Point{x: 10,y: 100});
+    window.change_pencil(2,Color{r: 0,g: 0,b: 255});
+    window.draw_rectangle(Point{x: 50,y: 50},50,50);
+    window.change_pencil(2,Color{r: 255,g: 0,b: 0});
+    window.draw_triangle(Point{x: 100,y: 50}, Point{x: 100,y: 100}, Point{x: 150,y: 100});
+
 
     window.get_input();
 }
--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,6 @@
 use std::ptr::{null, null_mut};
 use winapi::ctypes::c_int;
-use winapi::um::winuser::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetDC, GetMessageW, RegisterClassW, ReleaseDC, ShowWindow, TranslateMessage, UpdateWindow, MSG, SW_SHOW, WNDCLASSW, WS_OVERLAPPEDWINDOW};
+use winapi::um::winuser::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetDC, GetMessageW, RegisterClassW, ShowWindow, TranslateMessage, UpdateWindow, MSG, SW_SHOW, WNDCLASSW, WS_OVERLAPPEDWINDOW};
 use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::shared::windef::{HDC, HGDIOBJ, HPEN, HWND, LPPOINT};
 use winapi::shared::minwindef::{LRESULT, WPARAM, LPARAM, HINSTANCE};
@@ -11,7 +11,7 @@ extern "system" fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: L
 }
 fn to_w_string(s: &str) -> Vec<u16> {
     let mut v: Vec<u16> = s.encode_utf16().collect();
-    v.push(0); // Null-Terminierung
+    v.push(0);
     v
 }
 pub struct Point{
@@ -76,17 +76,22 @@ impl Window {
             UpdateWindow(self.hwnd);
         }
     }
-    pub fn draw_line(&self, start_point:&Point,end_point:Point) {
+    pub fn draw_line(&self, start_point:&Point,end_point:&Point) {
         unsafe {
             MoveToEx(self.hdc, start_point.x, start_point.y, 0 as LPPOINT);
             LineTo(self.hdc, end_point.x, end_point.y);
         }
     }
-    pub fn draw_rectangle(&self,start_point: &Point,width:u32,height:u32){
-        self.draw_line(start_point,Point{x:start_point.x + width as i32,y:start_point.y});
-        self.draw_line(start_point,Point{x:start_point.x,y:start_point.y + height as i32});
-        self.draw_line(&Point { x: start_point.x + width as i32, y: start_point.y }, Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
-        self.draw_line(&Point { x: start_point.x, y: start_point.y + height as i32},Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
+    pub fn draw_rectangle(&self,start_point:Point,width:u32,height:u32){
+        self.draw_line(&start_point, &Point { x: start_point.x + width as i32, y: start_point.y });
+        self.draw_line(&start_point, &Point { x: start_point.x, y: start_point.y + height as i32 });
+        self.draw_line(&Point { x: start_point.x + width as i32, y: start_point.y }, &Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
+        self.draw_line(&Point { x: start_point.x, y: start_point.y + height as i32},&Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
+    }
+    pub fn draw_triangle(&self,p1: Point,p2: Point,p3: Point){
+        self.draw_line(&p1, &p2);
+        self.draw_line(&p1, &p3);
+        self.draw_line(&p2, &p3);
     }
     pub fn change_pencil(&self, width:u32 ,color:Color){
         unsafe{
