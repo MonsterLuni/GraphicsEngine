e0700303cbf16ae9bcde5fa4ce587dc2a62435ff
MonsterLuni <monsterluni@gmail.com>
2024-10-30 20:11:02 +0100

    add change_pencil so now you can draw in any color you like + width is also modular

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod window;
 
-use crate::window::{Point, Window};
+use crate::window::{Color, Point, Window};
 
 fn main() {
     let window: Window = Window::new("Heureka".to_string(), "Class".to_string(), 100, 100, 200, 200);
-
-
     window.show();
-    window.draw_line(&Point{x: 20,y: 50},Point{x: 50,y: 50});
+
+    window.draw_line(&Point{x: 10,y: 50},Point{x: 10,y: 100});
+    window.change_pencil(2,Color{r: 255,g: 50,b: 20});
     window.draw_rectangle(&Point{x: 50,y: 50},50,50);
 
     window.get_input();
--- src/window.rs
+++ src/window.rs
@@ -2,9 +2,9 @@ use std::ptr::{null, null_mut};
 use winapi::ctypes::c_int;
 use winapi::um::winuser::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetDC, GetMessageW, RegisterClassW, ReleaseDC, ShowWindow, TranslateMessage, UpdateWindow, MSG, SW_SHOW, WNDCLASSW, WS_OVERLAPPEDWINDOW};
 use winapi::um::libloaderapi::GetModuleHandleW;
-use winapi::shared::windef::{HWND, LPPOINT};
+use winapi::shared::windef::{HDC, HGDIOBJ, HPEN, HWND, LPPOINT};
 use winapi::shared::minwindef::{LRESULT, WPARAM, LPARAM, HINSTANCE};
-use winapi::um::wingdi::{LineTo, MoveToEx};
+use winapi::um::wingdi::{CreatePen, DeleteObject, LineTo, MoveToEx, SelectObject, PS_SOLID, RGB};
 
 extern "system" fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
     unsafe { DefWindowProcW(hwnd, msg, w_param, l_param) }
@@ -18,6 +18,11 @@ pub struct Point{
     pub x: i32,
     pub y: i32
 }
+pub struct Color{
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
+}
 pub struct Window{
     name:String,
     class_name:String,
@@ -27,7 +32,8 @@ pub struct Window{
     width:u32,
     class:WNDCLASSW,
     h_instance:HINSTANCE,
-    hwnd: HWND
+    hwnd: HWND,
+    hdc: HDC
 }
 impl Window {
     pub fn new(name: String, class_name: String, pos_x: u32, pos_y: u32, height: u32, width: u32) -> Self {
@@ -46,7 +52,8 @@ impl Window {
         };
         unsafe { RegisterClassW(&class) };
         let hwnd:HWND = unsafe { CreateWindowExW(0, to_w_string(&class_name).as_ptr(), to_w_string(&name).as_ptr(), WS_OVERLAPPEDWINDOW, pos_x as c_int, pos_y as c_int, width as c_int, height as c_int, null_mut(), null_mut(), h_instance, null_mut()) };
-        Self { name, class_name, pos_x, pos_y, height, width, class, h_instance, hwnd }
+        let hdc = unsafe { GetDC(hwnd) };
+        Self { name, class_name, pos_x, pos_y, height, width, class, h_instance, hwnd, hdc }
     }
     pub fn show(&self) {
         unsafe {
@@ -71,10 +78,8 @@ impl Window {
     }
     pub fn draw_line(&self, start_point:&Point,end_point:Point) {
         unsafe {
-            let hdc = GetDC(self.hwnd);
-            MoveToEx(hdc, start_point.x, start_point.y, 0 as LPPOINT);
-            LineTo(hdc, end_point.x, end_point.y);
-            ReleaseDC(self.hwnd, hdc);
+            MoveToEx(self.hdc, start_point.x, start_point.y, 0 as LPPOINT);
+            LineTo(self.hdc, end_point.x, end_point.y);
         }
     }
     pub fn draw_rectangle(&self,start_point: &Point,width:u32,height:u32){
@@ -83,4 +88,14 @@ impl Window {
         self.draw_line(&Point { x: start_point.x + width as i32, y: start_point.y }, Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
         self.draw_line(&Point { x: start_point.x, y: start_point.y + height as i32},Point{x:start_point.x + width as i32,y:start_point.y + height as i32});
     }
+    pub fn change_pencil(&self, width:u32 ,color:Color){
+        unsafe{
+            let pen:HPEN = CreatePen(PS_SOLID as c_int, width as c_int, RGB(color.r, color.g, color.b));
+            let old_pen:HGDIOBJ = SelectObject(self.hdc,pen as _);
+            DeleteObject(old_pen);
+        };
+    }
+    /*pub fn fill(&self){
+
+    }*/
 }
